285106a5289db5d78e4db6e19339bb1e6b868329
Henrique Noronha Favorette <henrique.nofv@gmail.com>
2024-10-23 23:01:16 -0300

    add prop type to dish card

--- app/src/components/DishCard.vue
+++ app/src/components/DishCard.vue
@@ -1,8 +1,18 @@
-<script>
-export default {
+<script lang="ts">
+import { defineComponent,  } from 'vue';
+
+import type { PropType } from 'vue';
+
+type Dish = {
+  id: string
+  name: string,
+  status: string,
+}
+
+export default defineComponent({
   props: {
     dish: {
-      type: Object,
+      type: Object as PropType<Dish>,
       required: true,
     },
   },
@@ -26,7 +36,7 @@ export default {
       this.$emit('delete-dish', this.dish)
     },
   },
-}
+})
 </script>
 
 <template>
