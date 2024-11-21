2ddc653069f132197207338bb0b1173da11d84d7
Henrique Noronha Favorette <henrique.nofv@gmail.com>
2024-11-21 00:28:17 -0300

    refactor the nav bar to setup

--- app/src/components/TheNavbar.vue
+++ app/src/components/TheNavbar.vue
@@ -1,23 +1,29 @@
-<script>
-export default {
-  props: ['tagline'],
-  data: () => ({
-    navList: [
-      {
-        name: 'Home',
-        path: '/',
-      },
-      {
-        name: 'Restaurants',
-        path: '/restaurants',
-      },
-      {
-        name: 'Dishes',
-        path: '/dishes',
-      },
-    ],
-  }),
-}
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+defineProps({
+  tagline:  {
+    type: String,
+    required: true
+  }
+})
+
+const navList = ref([
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'Restaurants',
+    path: '/restaurants',
+  },
+  {
+    name: 'Dishes',
+    path: '/dishes',
+  },
+])
+
 </script>
 
 <template>
@@ -41,5 +47,3 @@ export default {
     </div>
   </nav>
 </template>
-
-<style></style>
